828de66a3ca30d4bd87d51d95d2620b816d7b2cf
nash300 <nadeesha300@yahoo.co.uk>
2025-03-03 19:50:19 +0100

    port choice page complete

--- src/pages/PortScreen.js
+++ src/pages/PortScreen.js
@@ -1,5 +1,28 @@
-import React from 'react'
+import React from "react";
+import "../App.css"; // Import custom styles
 
 export default function PortScreen() {
-  return <div>PortScreen</div>;
+  const nrOfPorts = 26; // Number of cards
+
+  return (
+    <div className="middle-container ">
+      <div className="container ">
+        <div className="row g-5 ">
+          {[...Array(nrOfPorts)].map((_, i) => (
+            <div key={i} className="col-lg-3 col-md-4 col-sm-6 d-flex ">
+              {/* Entire Card is a Clickable Button */}
+              <button
+                className="card port-card shadow-lg w-100 rounded-pill  "
+                onClick={() => alert(`Port ${i + 1} Selected`)} // TO-DO
+              >
+                <div className="card-body text-center ">
+                  <h3 className="card-title"> {i + 1}</h3>
+                </div>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
